38c7edae7f9a35007fb437243caf6e3d665c8482
anushreesrinivas18 <135025522+anushreesrinivas18@users.noreply.github.com>
2023-07-31 08:45:02 +0530

    Add files via upload

---
+++ if.js
@@ -0,0 +1,20 @@
+a=20
+if (a>80)
+    {
+        console.log("A grade");
+    }
+    else if (a>60)
+    {
+        console.log("B grade")
+    }
+    else if (a>50)
+    {
+        console.log("C grade")
+    }
+    else if (a>40)
+    {
+        console.log("D grade")
+    }
+    else{
+        
+    }
